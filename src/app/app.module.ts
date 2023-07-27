315b5a73fdc44f6949ea86c1481a407991a16db9
abhinav101901 <abhinav8299233838@gmail.com>
2023-07-27 23:39:23 +0530

    sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PageComponent } from './page/page.component';
+import { SidebarComponent } from './page/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/page/sidebar/sidebar.component.ts
@@ -0,0 +1,41 @@
+import { Component, AfterViewInit } from '@angular/core';
+import * as $ from 'jquery'; // Import jQuery here
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent implements AfterViewInit {
+  ngAfterViewInit() {
+    // jQuery code here
+    $(".sidebar-dropdown > a").click(function() {
+      $(".sidebar-submenu").slideUp(200);
+      if (
+        $(this)
+          .parent()
+          .hasClass("active")
+      ) {
+        $(".sidebar-dropdown").removeClass("active");
+        $(this)
+          .parent()
+          .removeClass("active");
+      } else {
+        $(".sidebar-dropdown").removeClass("active");
+        $(this)
+          .next(".sidebar-submenu")
+          .slideDown(200);
+        $(this)
+          .parent()
+          .addClass("active");
+      }
+    });
+
+    $("#close-sidebar").click(function() {
+      $(".page-wrapper").removeClass("toggled");
+    });
+    $("#show-sidebar").click(function() {
+      $(".page-wrapper").addClass("toggled");
+    });
+  }
+}
